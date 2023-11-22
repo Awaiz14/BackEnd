c239e3d318a52536e416238dc5b7185bdaaf10e4
Awaiz14 <100952563+Awaiz14@users.noreply.github.com>
2023-11-22 13:58:38 +0000

    Add files via upload

--- DigitalWebsite/AddClass.php
+++ DigitalWebsite/AddClass.php
@@ -16,16 +16,28 @@ if ($conn->connect_error) {
 $className = $_POST['className'];
 $teacherID = $_POST['teacherID'];
 $classCapacity = $_POST['classCapacity'];
+
+// Check if the teacher ID already exists in the database
+$query = "SELECT * FROM classes WHERE teacherID = '$teacherID'";
+$result = mysqli_query($conn, $query);
+// If a row is returned, it means the teacher ID is already assigned to a class
+if (mysqli_num_rows($result) > 0) {
+   echo "The teacher is already assigned to another class<br>";
+   echo '<button onclick="history.go(-1)">Go Back</button>';
+   // You can also redirect the user back to the form or display an error message
+   
+} else {
  
     // Insert data into the "pupils" table
     $sql = "INSERT INTO classes (className, teacherID, classCapacity) VALUES ('$className', '$teacherID', '$classCapacity')";
  
     if ($conn->query($sql) === TRUE) {
-        echo "Data inserted successfully";
+        echo "Data inserted successfully<br>";
+        echo '<a href="AddClass.html"><button>Go Back</button></a>';
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
- 
+}
  
 // Close connection
 $conn->close();
