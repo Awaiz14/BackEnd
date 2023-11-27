e29aa6798c99ac02025b6ece990ca9ce8f8d86ef
Awaiz14 <100952563+Awaiz14@users.noreply.github.com>
2023-11-27 10:54:33 +0000

    Add files via upload
    Added in View TeacherAssistants & Add TeacherAssistants

---
+++ DigitalWebsite/ViewTeacherassistant.php
@@ -0,0 +1,35 @@
+<?php
+// Database connection parameters
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$database = "school";
+ 
+// Creates the connection using parameters
+$conn = new mysqli($servername, $username, $password, $database);
+ 
+// Checks connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+   // Retrieve teachers' information from the database
+   $query = "SELECT assistantID, assistantTitle, assistantName, assistantSurname, assistantDOB, 
+   assistantEmail, assistantPhone, assistantAddress, teacherID FROM teacherassistants";
+   $result = mysqli_query($conn, $query);
+   // Display the retrieved information in the HTML table
+   while ($row = mysqli_fetch_assoc($result)) {
+       echo "<tr>";
+       echo "<td>" . $row['assistantID'] . "</td>";
+       echo "<td>" . $row['assistantTitle'] . "</td>";
+       echo "<td>" . $row['assistantName'] . "</td>";
+       echo "<td>" . $row['assistantSurname'] . "</td>";
+       echo "<td>" . $row['assistantDOB'] . "</td>";
+       echo "<td>" . $row['assistantEmail'] . "</td>";
+       echo "<td>" . $row['assistantPhone'] . "</td>";
+       echo "<td>" . $row['assistantAddress'] . "</td>";
+       echo "<td>" . $row['teacherID'] . "</td>";
+       echo "</tr>";
+   }
+   // Close the database connection
+   mysqli_close($conn);
+?>
