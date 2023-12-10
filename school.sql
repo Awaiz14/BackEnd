052963c552e990f463eb1a894309f81dcb2e6a39
Awaiz14 <100952563+Awaiz14@users.noreply.github.com>
2023-12-10 22:10:15 +0000

    Add files via upload
    Latest school DB

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2023 at 10:01 PM
+-- Generation Time: Dec 10, 2023 at 11:09 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,9 +30,28 @@ SET time_zone = "+00:00";
 CREATE TABLE `attendance` (
   `studentID` varchar(50) NOT NULL,
   `attendanceDate` date NOT NULL,
+  `studentName` varchar(50) DEFAULT NULL,
+  `studentSurname` varchar(50) DEFAULT NULL,
+  `className` varchar(50) DEFAULT NULL,
   `attendanceStatus` varchar(50) DEFAULT NULL,
   `attendanceNotes` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`studentID`, `attendanceDate`, `studentName`, `studentSurname`, `className`, `attendanceStatus`, `attendanceNotes`) VALUES
+('S001', '2023-12-04', 'Harry', 'Potter', 'Year1', 'Absent', 'Rang in sick'),
+('S001', '2023-12-08', 'Harry', 'Potter', 'Year1', 'Late', 'traffic'),
+('S001', '2023-12-10', 'Harry', 'Potter', 'Year1', 'Absent', 'Sick'),
+('S002', '2023-12-04', 'Sammy', 'Doe', 'Year1', 'Late', 'Train delayed'),
+('S002', '2023-12-10', 'Sammy', 'Doe', 'Year3', 'Present', ''),
+('S003', '2023-12-04', 'Steven', 'Richard', 'Reception', 'Late', 'Train delays'),
+('S003', '2023-12-05', 'Steven', 'Richard', 'Reception', 'Late', ''),
+('S003', '2023-12-10', 'Steven', 'Richard', 'Reception', 'Late', 'Had appointment'),
+('S004', '2023-12-10', 'Harry', 'Maguire', 'Year6', 'Late', 'Hospital appointment'),
+('S007', '2023-12-10', 'Liam', 'Smith', 'Year4', 'Present', '');
 
 -- --------------------------------------------------------
 
@@ -51,9 +70,13 @@ CREATE TABLE `classes` (
 --
 
 INSERT INTO `classes` (`className`, `teacherID`, `classCapacity`) VALUES
-('Reception', 'T001', 2),
-('Year1', 'T003', 15),
-('Year2', 'T004', 15);
+('Reception', 'T001', 30),
+('Year1', 'T003', 16),
+('Year2', 'T004', 15),
+('Year3', 'T006', 20),
+('Year4', 'T002', 10),
+('Year5', 'T007', 22),
+('Year6', 'T005', 14);
 
 -- --------------------------------------------------------
 
@@ -78,7 +101,14 @@ INSERT INTO `parents` (`parentID`, `parentName`, `parentSurname`, `parentEmail`,
 ('', NULL, NULL, NULL, NULL, NULL),
 ('P001', 'James', 'Potter', 'Snapesbully@gmail.com', '07329183728', '81 Sandon Road, Stafford, England, ST16 3HF'),
 ('P002', 'Lily', 'Potter', 'motherseyes@yahoo.com', '07836472822', '81 Sandon Road, Stafford, England, ST16 3HF'),
-('P003', 'Mother', 'Teresa3', 'hello@gmail', 'test', 'test');
+('P003', 'Jannet', 'Doe', 'jannet@gmail.com', '07217199017', '5 Churn Close, Crudgington, England, TF6 6FE'),
+('P004', 'John', 'Richard', 'Lennoncakes@hotmail.com', '07211524719', '42 Belmont Drive, Staveley, England, S43 3PQ'),
+('P005', 'Emily', 'Maguire', 'emmie@mufc.com', '07524362842', '1 Monument View, Polesworth, England, B78 1NF'),
+('P006', 'Maisie', 'Richard', 'Maisielazy@btnet.com', '07662553917', '42 Belmont Drive, Staveley, England, S43 3PQ'),
+('P007', 'Rose', 'Jane', 'Rosie@hotmail.com', '07329428821', '80 Higher Market Street, Farnworth, England, BL4 9BB'),
+('P008', 'Dave', 'Tielman', 'Dave63@gmail.com', '07826528572', '16 Back Mowbray Terrace, Choppington, England, NE62 5QH'),
+('P009', 'Jude', 'Smith', 'Judey@hotmail.co.uk', '07847262745', '104 Caldey Place, Blaenymaes, England, SA5 5PN'),
+('P010', 'Cristian', 'Smith', 'Cristy241@hotmail.com', '07254781187', '104 Caldey Place, Blaenymaes, England, SA5 5PN');
 
 -- --------------------------------------------------------
 
@@ -100,7 +130,12 @@ CREATE TABLE `salaries` (
 
 INSERT INTO `salaries` (`staffID`, `staffRole`, `staffLevel`, `annualPay`, `payFrequency`) VALUES
 ('T001', 'Teacher', '3', 40000, 'Monthly'),
-('TA002', 'Assistant', '2', 20001, 'Weekly');
+('T002', 'Teacher', '1', 24500, 'Monthly'),
+('T003', 'Teacher', '2', 32000, 'Monthly'),
+('T004', 'Teacher', '3', 35000, 'Monthly'),
+('TA002', 'Assistant', '1', 20000, 'Weekly'),
+('TA003', 'Assistant', '1', 18000, 'Monthly'),
+('TA004', 'Assistant', '3', 24000, 'Weekly');
 
 --
 -- Triggers `salaries`
@@ -147,8 +182,13 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`studentID`, `studentName`, `studentSurname`, `studentDOB`, `studentEmail`, `studentAddress`, `className`, `teacherID`, `parent1ID`, `parent2ID`, `medicalInfo`) VALUES
-('S001', 'Harry', 'Potter', '2019-09-01', 'HarryS001@alphonsus.ac.uk', '81 Sandon Road, Stafford, England, ST16 3HF', 'Year1', 'T003', 'P001', '', ''),
-('S002', 'Sammy', 'Doe', '2015-06-14', 'JohnnyS002@alphonsus.ac.uk', '5 Churn Close, Crudgington, England, TF6 6FE', 'Year2', 'T004', 'P002', '', 'mental ill person');
+('S001', 'Harry', 'Potter', '2019-09-01', 'HarryS001@alphonsus.ac.uk', '81 Sandon Road, Stafford, England, ST16 3HF', 'Year1', 'T003', 'P001', 'P002', ''),
+('S002', 'Sammy', 'Doe', '2015-06-14', 'JohnnyS002@alphonsus.ac.uk', '5 Churn Close, Crudgington, England, TF6 6FE', 'Year3', 'T006', 'P003', '', 'Has Asthma'),
+('S003', 'Steven', 'Richard', '2015-04-15', 'StevenS003@alphonsus.ac.uk', '42 Belmont Drive, Staveley, England, S43 3PQ', 'Reception', 'T001', 'P004', 'P006', 'Allergic to walnuts'),
+('S004', 'Harry', 'Maguire', '2013-02-06', 'HarryS004@alphonsus.ac.uk', '1 Monument View, Polesworth, England, B78 1NF', 'Year6', 'T005', 'P005', '', ''),
+('S005', 'Mary', 'Jane', '2013-10-10', 'MaryS005@alphonsus.ac.uk', '80 Higher Market Street, Farnworth, England, BL4 9BB', 'Year5', 'T007', 'P007', '', ''),
+('S006', 'Bethany', 'Tielman', '2017-05-16', 'BethanyS006@alphonsus.ac.uk', '16 Back Mowbray Terrace, Choppington, England, NE62 5QH', 'Year2', 'T004', 'P008', '', 'Partially blind'),
+('S007', 'Liam', 'Smith', '2015-03-27', 'LiamS007@alphonsus.ac.uk', '104 Caldey Place, Blaenymaes, England, SA5 5PN', 'Year4', 'T002', 'P010', 'P009', 'Allergic to wheat');
 
 -- --------------------------------------------------------
 
@@ -166,7 +206,7 @@ CREATE TABLE `teacherassistants` (
   `assistantPhone` varchar(50) DEFAULT NULL,
   `assistantAddress` varchar(100) DEFAULT NULL,
   `teacherID` varchar(50) DEFAULT NULL,
-  `assistantSalary` varchar(50) DEFAULT NULL
+  `assistantSalary` decimal(10,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -174,7 +214,10 @@ CREATE TABLE `teacherassistants` (
 --
 
 INSERT INTO `teacherassistants` (`assistantID`, `assistantTitle`, `assistantName`, `assistantSurname`, `assistantDOB`, `assistantEmail`, `assistantPhone`, `assistantAddress`, `teacherID`, `assistantSalary`) VALUES
-('TA001', 'Mr', 'testworking', 'testworking', '2000-12-14', 'test@gmail.com', 'test', 'test', 'T001', NULL);
+('TA001', 'Mr', 'Ed', 'Warren', '1970-09-04', 'Warrenman@conjuring.com', '07392748194', '29 Greenwood Close, Birmingham, England, B14 6ET', 'T004', NULL),
+('TA002', 'Mr', 'Robert', 'Lewandowski', '1980-09-17', 'Rl9@polski.com', '07947284281', '34 Charnwood Crescent, Newton, England, DE55 5SB', 'T007', 20000),
+('TA003', 'Mrs', 'Emma', 'Watson', '1982-02-16', 'Emmaw@gmail.com', '07362582917', '92 Huntington Road, York, England, YO31 8RN', 'T005', 18000),
+('TA004', 'Miss', 'Zara', 'Larsson', '1992-01-01', 'ZaraLars@yahoo.com', '07462184763', '1 Pengeulan, Dolwyddelan, England, LL25 0UQ', 'T001', 24000);
 
 -- --------------------------------------------------------
 
@@ -191,7 +234,7 @@ CREATE TABLE `teachers` (
   `teacherEmail` varchar(100) DEFAULT NULL,
   `teacherPhone` varchar(50) DEFAULT NULL,
   `teacherAddress` varchar(100) DEFAULT NULL,
-  `teacherSalary` varchar(10) DEFAULT NULL
+  `teacherSalary` decimal(10,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -199,9 +242,13 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`teacherID`, `teacherTitle`, `teacherName`, `teacherSurname`, `teacherDOB`, `teacherEmail`, `teacherPhone`, `teacherAddress`, `teacherSalary`) VALUES
-('T001', 'Miss', 'Wanda', 'Maximoff', '1998-06-23', 'Scarletwitch@gmail.com', '07937622864', '87 Railway Road, Downham, England, PE38 9EL', '40000'),
-('T003', 'Mr', 'Peter', 'Parker', '1998-09-13', 'Spidey@webs.co.uk', '07663297562', '66 Park Lane, London, England, OX42 5BE', NULL),
-('T004', 'Mrs', 'Lorraine', 'Warren', '1976-12-14', 'Iseeghosts@conjuring.com', '07328427182', '36 Buxted Road, Kirkby, England, L32 6SQ', NULL);
+('T001', 'Miss', 'Wanda', 'Maximoff', '1990-06-23', 'Scarletwitch@gmail.com', '07937622864', '87 Railway Road, Downham, England, PE38 9EL', 40000),
+('T002', 'Miss', 'Bella', 'Swan', '1955-04-18', 'omgedward@yahoo.com', '07284729645', '6 Kings Pool Walk, York, England, YO1 7NA', 24500),
+('T003', 'Mr', 'Peter', 'Parker', '1998-09-13', 'Spidey@webs.com', '07663297562', '66 Park Lane, London, England, OX42 5BE', 32000),
+('T004', 'Mrs', 'Lorraine', 'Warren', '1976-12-14', 'Iseeghosts@conjuring.com', '07328427182', '36 Buxted Road, Kirkby, England, L32 6SQ', 35000),
+('T005', 'Mr', 'Michael', 'Scofield', '1982-03-29', 'prisonbreaker@gmail.com', '07381942754', '49A Lytton Grove, London, England, SW15 2HD', NULL),
+('T006', 'Mrs', 'Ariana', 'Grande', '1963-10-06', 'Arianboo@hotmail.co.uk', '07518427491', '18 Almond Drive, Plymouth, England, PL7 2WY', NULL),
+('T007', 'Mr', 'Cristiano', 'Ronaldo', '1987-07-07', 'Cr7@ronaldo.com', '07927644255', '3 Brow Street, Maryport, England, CA15 6EF', NULL);
 
 --
 -- Indexes for dumped tables
